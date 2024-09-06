89b8f8524c8a047ac7f7ec489e19664f4e371a0c
Ahimsa Jenar Bramsaifstyo Kusuma <tio.rmx@gmail.com>
2024-09-06 16:34:18 +0700

    add constructor

--- Praktikum/Praktikum/Program.cs
+++ Praktikum/Praktikum/Program.cs
@@ -5,11 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Mobil mobil1 = new Mobil();
-        Mobil mobil2 = new Mobil();
+        Mobil mobil1 = new Mobil("Toyota", "Canny", 2021);
+        Mobil mobil2 = new Mobil("Honda", "Jazz", 1998);
 
-        mobil1.tampilkanMobil("Toyota","Canny",2021);
-        mobil2.merk = "Honda";
+        //mobil1.tampilkanMobil("Toyota", "Canny", 2021);
+        //mobil2.merk = "Honda";
+        //Console.WriteLine(mobil1.merk);
+
+        mobil1.tampilkanMobil();
+        Console.WriteLine("========");
+        mobil2.tampilkanMobil();
     }
 }
 
@@ -17,11 +22,25 @@ class Mobil
 {
     public string merk, model;
     public int tahun;
-    public string kata = "Mobil Baru";
 
-    public void tampilkanMobil(string merk,string model,int tahun) {
-        Console.WriteLine("Merk : " + merk);
-        Console.WriteLine($"Model : {model}");
-        Console.WriteLine($"Tahun : {tahun}");
+    public Mobil(string merk, string model, int tahun)
+    {
+        this.merk = merk;
+        this.model = model;
+        this.tahun = tahun;
+    }
+
+    //    public void tampilkanMobil(string merk,string model,int tahun) {
+    //        Console.WriteLine("Merk : " + merk);
+    //        Console.WriteLine($"Model : {model}");
+    //        Console.WriteLine($"Tahun : {tahun}");
+    //    }
+    //}
+
+    public void tampilkanMobil()
+    {
+        Console.WriteLine("Merk : " + this.merk);
+        Console.WriteLine("Model : " + this.model);
+        Console.WriteLine($"Tahun : " +this.tahun);
     }
 }
